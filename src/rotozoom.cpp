10567ae3c93f173c1e0670212911cc2ea82a942a
Johan Gardhage <johan.gardhage@gmail.com>
2023-08-03 13:18:00 +0200

    Simplify rotation formula

--- src/rotozoom.cpp
+++ src/rotozoom.cpp
@@ -10,29 +10,23 @@
 #define TEXTURE_HEIGHT 256
 #define SINE_VALUES 1024
 
-float SinTable[SINE_VALUES];
-
 void DEMO_Render(double deltatime)
 {
 	// Calculate frame
-	static double frame_counter = 0;
-	frame_counter += deltatime * 200;
-	int frame = frame_counter;
+	static double angle = 0;
+	angle += deltatime * 100;
 
 	unsigned char *image = RETRO_GetTextureImage();
 
 	// Calculate movement
-	float xstep = SinTable[frame % SINE_VALUES];
-	float ystep = SinTable[(frame + 256) % SINE_VALUES];
-	float zstep = SinTable[frame % SINE_VALUES];
-	float xd = xstep * zstep;
-	float yd = ystep * zstep;
+	float sina = sin(angle * M_PI / 180.0f);
+	float cosa = cos(angle * M_PI / 180.0f);
 
 	// Draw texture
 	for (int y = 0; y < RETRO_HEIGHT; y++) {
 		for (int x = 0; x < RETRO_WIDTH; x++) {
-			int tx = (unsigned int)(yd * y - xd * x) % TEXTURE_WIDTH;
-			int ty = (unsigned int)(xd * y + yd * x) % TEXTURE_HEIGHT;
+			int tx = (unsigned int)(( x * cosa - y * sina) * (sina + 1)) % TEXTURE_WIDTH;
+			int ty = (unsigned int)(( x * sina + y * cosa) * (sina + 1)) % TEXTURE_HEIGHT;
 			int color = image[ty * TEXTURE_WIDTH + tx];
 
 			RETRO_PutPixel(x, y, color);
@@ -44,9 +38,4 @@ void DEMO_Initialize(void)
 {
 	RETRO_LoadTexture("assets/rotozoom_256x256.pcx");
 	RETRO_SetPaletteFromTexture();
-
-	// Init sine table
-	for (int i = 0; i < SINE_VALUES; i++) {
-		SinTable[i] = sin(i * M_PI / 512);
-	}
 }

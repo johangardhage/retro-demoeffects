532b220df9bf567e8656e8bcb916167405d8279a
Johan Gardhage <johan.gardhage@gmail.com>
2023-08-19 13:21:51 +0200

    Add metaballs demo

--- src/metaballs.cpp
+++ src/metaballs.cpp
@@ -4,70 +4,62 @@
 // Author: Johan Gardhage <johan.gardhage@gmail.com>
 //
 #include "lib/retro.h"
+#include "lib/retrogfx.h"
 #include "lib/retromain.h"
 
-#define NUM_BALLS 4
-#define SINE_VALUES 360
+#define NUM_BALLS 5
+#define THRESHOLD 50
 
 struct MetaBall {
-	float x, y, r;
+	Point2Df pos;
+	Point2Df vel;
+	float radius;
 } Balls[NUM_BALLS];
-
-float SinTable[SINE_VALUES];
-float CosTable[SINE_VALUES];
 
 void DEMO_Render(double deltatime)
 {
-	// Calculate frame
-	static double frame_counter = 0;
-	frame_counter += deltatime * 100;
-	int frame = (int)frame_counter % SINE_VALUES;
+	// Draw balls
+	for (int x = 0; x < RETRO_WIDTH; x++) {
+		for (int y = 0; y < RETRO_HEIGHT; y++) {
+			float sum = 0;
+			// Calculate iso-surface
+			for (int i = 0; i < NUM_BALLS; i++) {
+				float a = x - Balls[i].pos.x;
+				float b = y - Balls[i].pos.y;
+				float d = sqrt(a * a + b * b); // calculate pixel distance from metaball position
+				sum += 50 * Balls[i].radius / d;
+			}
+			// Threshold
+			if (sum >= THRESHOLD) {
+				RETRO_PutPixel(x, y, 255);
+			}
+		}
+	}
 
 	// Move balls
-	Balls[0].r = 1000;
-	Balls[0].x = CosTable[frame] * -100 + (RETRO_WIDTH / 2);
-	Balls[0].y = SinTable[frame] * -10 + (RETRO_HEIGHT / 2);
-	Balls[1].r = 4000;
-	Balls[1].x = CosTable[frame] * 10 + (RETRO_WIDTH / 2);
-	Balls[1].y = SinTable[frame] * 60 + (RETRO_HEIGHT / 2);
-	Balls[2].r = 7000;
-	Balls[2].x = CosTable[frame] * -130 + (RETRO_WIDTH / 2);
-	Balls[2].y = SinTable[frame] * -80 + (RETRO_HEIGHT / 2);
-	Balls[3].r = 10000;
-	Balls[3].x = CosTable[frame] * -80 + (RETRO_WIDTH / 2);
-	Balls[3].y = SinTable[frame] * 70 + (RETRO_HEIGHT / 2);
-
-	// Draw balls
-	for (int y = 0; y < RETRO_HEIGHT; y++) {
-		for (int x = 0; x < RETRO_WIDTH; x++) {
-			float color = 0;
-			for (int i = 0; i < NUM_BALLS; i++) {
-				color += Balls[i].r / float ((x - Balls[i].x) * (x - Balls[i].x) + (y - Balls[i].y) * (y - Balls[i].y));
-			}
-			color = CLAMP256(20 * color);
-
-			RETRO_PutPixel(x, y, color);
+	for (int i = 0; i < NUM_BALLS; i++) {
+		if (Balls[i].pos.x < 0 || Balls[i].pos.x > RETRO_WIDTH) {
+			Balls[i].vel.x *= -1;
 		}
+		if (Balls[i].pos.y < 0 || Balls[i].pos.y > RETRO_HEIGHT) {
+			Balls[i].vel.y *= -1;
+		}
+		Balls[i].pos.x += Balls[i].vel.x;
+		Balls[i].pos.y += Balls[i].vel.y;
 	}
 }
 
 void DEMO_Initialize(void)
 {
-	// Init tables
-	for (int i = 0; i < SINE_VALUES; i++) {
-		SinTable[i] = sin(i / 180.0 * M_PI);
-		CosTable[i] = cos(i / 180.0 * M_PI);
-	}
-
 	// Init palette
-	int light = 350;
-	int reflect = 130;
-	int ambient = 0;
-	for (int i = 0; i < RETRO_COLORS; i++) {
-		double intensity = cos((255 - i) / 512.0 * M_PI);
-		int r = CLAMP256(63 * ambient / 255.0 + 63 * intensity + pow(intensity, reflect) * light);
-		int g = CLAMP256(72 * ambient / 255.0 + 72 * intensity + pow(intensity, reflect) * light);
-		int b = CLAMP256(128 * ambient / 255.0 + 128 * intensity + pow(intensity, reflect) * light);
-		RETRO_SetColor(i, r, g, b);
+	RETRO_SetColor(255, 255, 255, 255);
+
+	// Init balls
+	for (int i = 0; i < NUM_BALLS; i++) {
+		Balls[i].pos.x = rand() % RETRO_WIDTH;
+		Balls[i].pos.y = rand() % RETRO_HEIGHT;
+		Balls[i].vel.x = (float)rand() / RAND_MAX * 2;
+		Balls[i].vel.y = (float)rand() / RAND_MAX * 2;
+		Balls[i].radius = rand() % 20 + 10;
 	}
 }

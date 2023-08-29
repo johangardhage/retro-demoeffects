cc1bb103a4f3be6553e39e2accc4c9a00407f1a3
Johan Gardhage <johan.gardhage@gmail.com>
2023-08-29 12:43:40 +0200

    Add font

--- src/font.cpp
+++ src/font.cpp
@@ -9,8 +9,14 @@
 
 void DEMO_Render(double deltatime)
 {
-	char str[] = { "ABCDEFGHIJKLMNOPQRSTUVWXYZ!?" };
-	RETRO_PutString(str, 10, 10, 255);
+	char str1[] = { "ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
+	char str2[] = { "abcdefgjijklmnopqrstuvwzyz" };
+	char str3[] = { "0123456789" };
+	char str4[] = { "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`" };
+	RETRO_PutString(str1, 10, 10, 255);
+	RETRO_PutString(str2, 10, 20, 255);
+	RETRO_PutString(str3, 10, 30, 255);
+	RETRO_PutString(str4, 10, 40, 255);
 }
 
 void DEMO_Initialize(void)

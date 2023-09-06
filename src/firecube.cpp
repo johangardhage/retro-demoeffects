a9a6485e83bed5095e74414ed929a0ee9ea5e87a
Johan Gardhage <johan.gardhage@gmail.com>
2023-09-06 10:17:12 +0200

    Remove old demos

--- src/firecube.cpp
+++ src/firecube.cpp
@@ -21,17 +21,7 @@ void DEMO_Render2(double deltatime)
 	RETRO_SortAllFaces();
 	RETRO_RenderModel(RETRO_POLY_WIREFIRE);
 
-	unsigned char *burn = RETRO_FrameBuffer();
-
-	for (int a = 0; a < RETRO_WIDTH; a++) {
-		for (int b = 0; b < RETRO_HEIGHT - 1; b++) {
-			if (burn[a + (b + 1) * RETRO_WIDTH] > 0 || burn[a + b * RETRO_WIDTH] > 0)
-				burn[a + b * RETRO_WIDTH] = ((burn[(a - 1) + (b + 1) * RETRO_WIDTH] + burn[(a + 1) + (b + 1) * RETRO_WIDTH] + burn[a + (b + 1) * RETRO_WIDTH] + burn[a + (b + 2) * RETRO_WIDTH]) >> 2);
-			if (burn[a + b * RETRO_WIDTH] > 3)
-				burn[a + b * RETRO_WIDTH] -= 2;
-		}
-	}
-
+	RETRO_Blur(RETRO_BLUR_8, 3);
 	RETRO_Flip();
 }
 
@@ -39,19 +29,19 @@ void DEMO_Initialize(void)
 {
 	// Init palette
 	unsigned char r = 0, g = 0, b = 0;
-	for (int i = 0; i < 256; i++)
+	for (int i = 0; i < 256; i++) {
 		RETRO_SetColor(i, 0, 0, 0);
-
+	}
 	for (int i = 0; i < 63; i++) {
-		RETRO_SetColor(i, r, 0, 0);
+		RETRO_SetColor(i, r * 4, 0, 0);
 		r++;
 	}
 	for (int i = 63; i < 127; i++) {
-		RETRO_SetColor(i, 63, g, 0);
+		RETRO_SetColor(i, 63 * 4, g * 4, 0);
 		g++;
 	}
 	for (int i = 127; i < 190; i++) {
-		RETRO_SetColor(i, 63, 63, b);
+		RETRO_SetColor(i, 63 * 4, 63 * 4, b * 4);
 		b++;
 	}
 
